f7da7e47ea5e8271e935d99ac897d4167e2fd6cf
CherylChen06 <cheryllech@gmail.com>
2026-03-25 16:41:53 -0400

    score.c fixed

--- source/score.c
+++ source/score.c
@@ -30,6 +30,10 @@ int main() {
     draw_staff(15, 30);
 	draw_time_signature(32, 30);
     draw_toolbar();
+    draw_staff(15, 90);
+	draw_staff(15, 150);
+	draw_bar_line(HOR_MAX - 15 - 2, 150);
+	draw_bar_line(HOR_MAX - 15 - 4, 150);
 	
 	wait_for_vsync(); // swap front and back buffers on VGA vertical sync
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
@@ -63,158 +67,103 @@ int main() {
 }
 
 void draw_score(Score* score){
-    // handles drawing the whole score on the page
+	// handles drawing the whole score on the page
     // draws the score using different functions for drawing notes
     // draw each note here and call it once in the main function
-
     for (int note_idx = 0; note_idx < 64; note_idx++) { // note_idx for note array
         // read the duration here instead of edge_cap
-        Note* current_note = score.notes[note_idx];
-        char duration = current_note.duration;
-        char pitch = current_note.note;
-        int octave = current_note.octave;
-
-        if (pitch == '\0') break;
-        
-        note_drawn = true;
-        
-		if (duration == 'w') {
-			note_type = 'w';
-		}	
-        else if (duration == 'h') {
-			note_type = 'h';	
-		}
-		else if (duration == 'q') {
-			note_type = 'q';	
-		}
-		else if (duration == 'e') {
-			note_type = 'e';
-		}
-		else if (duration == 's') {
-			note_type = 's';
-		}
-				
-        // read note here (note and octave together)
-		if ((pitch == 'C') && (octave == 4)) { // C4
-			current_vert = staff_center + 27;
-            // idea for accidentals put accidental as member of Note struct
-            // read if sharp or flat and draw those --> need graphics for this
-		}	
-		if ((pitch == 'D') && (octave == 4)) { // D4
-			current_vert = staff_center + 23;
-	    }
-		if ((pitch == 'E') && (octave == 4)) { // E4
-			current_vert = staff_center + 18;
-		}	
-		if ((pitch == 'F') && (octave == 4)) { // F4
-			current_vert = staff_center + 13;
-		}
-		if ((pitch == 'G') && (octave == 4)) { // G4
-			current_vert = staff_center + 9;
-		}
-		if ((pitch == 'A') && (octave == 4)) { // A4
-			current_vert = staff_center + 5;
-		}
-		if ((pitch == 'B') && (octave == 4)) { // B4
-			current_vert = staff_center;
-		}
-		if ((pitch == 'C') && (octave == 5)) { // C5
-			current_vert = staff_center - 5;
-		}
-        if ((pitch == 'D') && (octave == 5)) { // D5
-			current_vert = staff_center - 9;
-	    }
-		if ((pitch == 'E') && (octave == 5)) { // E5
-			current_vert = staff_center - 14;
-		}	
-		if ((pitch == 'F') && (octave == 5)) { // F5
-			current_vert = staff_center - 18;
-		}
-		if ((pitch == 'G') && (octave == 5)) { // G5
-			current_vert = staff_center - 23;
-		}
-		if ((pitch == 'A') && (octave == 5)) { // A5
-			current_vert = staff_center - 27;
-		}
-		if ((pitch == 'B') && (octave == 5)) { // B5
-			current_vert = staff_center - 32;
-		}
-		if ((pitch == 'C') && (octave == 6)) { // C6
-			current_vert = staff_center - 36;
-		}
-    }
-    // end checking note and octave
-        
-    // keep this
-	draw_staff(15, 30);
-	draw_time_signature(32, 30);
-	draw_toolbar();
+		Note* current_note = &score->notes[note_idx];
+        char duration = current_note->duration;
+        char pitch = current_note->note;
+        int octave = current_note->octave;
 		
-    // keep note_drawn thing
-    // add more notes to this
-	if (note_drawn) {
-        // add ledger line function for anything that needs it
-        if ((current_vert < staff_center - 23) || (current_vert > staff_center + 23)) {
-            draw_ledger_line(current_hor, current_vert);
+        if (pitch == '\0') break;
+
+        note_drawn = true;
+        if      (duration == 'w') note_type = 'w';
+        else if (duration == 'h') note_type = 'h';
+        else if (duration == 'q') note_type = 'q';
+        else if (duration == 'e') note_type = 'e';
+        else if (duration == 's') note_type = 's';
+		
+		// read note here (note and octave together)
+        if      ((pitch == 'C') && (octave == 4)) current_vert = staff_center + 27;
+        // idea for accidentals put accidental as member of Note struct
+        // read if sharp or flat and draw those --> need graphics for this
+		else if ((pitch == 'D') && (octave == 4)) current_vert = staff_center + 23;
+        else if ((pitch == 'E') && (octave == 4)) current_vert = staff_center + 18;
+        else if ((pitch == 'F') && (octave == 4)) current_vert = staff_center + 13;
+        else if ((pitch == 'G') && (octave == 4)) current_vert = staff_center + 9;
+        else if ((pitch == 'A') && (octave == 4)) current_vert = staff_center + 5;
+        else if ((pitch == 'B') && (octave == 4)) current_vert = staff_center;
+        else if ((pitch == 'C') && (octave == 5)) current_vert = staff_center - 5;
+        else if ((pitch == 'D') && (octave == 5)) current_vert = staff_center - 9;
+        else if ((pitch == 'E') && (octave == 5)) current_vert = staff_center - 14;
+        else if ((pitch == 'F') && (octave == 5)) current_vert = staff_center - 18;
+        else if ((pitch == 'G') && (octave == 5)) current_vert = staff_center - 23;
+        else if ((pitch == 'A') && (octave == 5)) current_vert = staff_center - 27;
+        else if ((pitch == 'B') && (octave == 5)) current_vert = staff_center - 32;
+        else if ((pitch == 'C') && (octave == 6)) current_vert = staff_center - 36;
+		// end checking note and octave
+		
+		// keep this
+        draw_staff(15, 30);
+        draw_time_signature(32, 30);
+        draw_toolbar();
+		draw_staff(15, 90);
+		draw_staff(15, 150);
+		draw_bar_line(HOR_MAX - 15 - 2, 150);
+		draw_bar_line(HOR_MAX - 15 - 4, 150);
+		
+		// keep note_drawn thing
+    	// add more notes to this
+        if (note_drawn) {
+			// add ledger line function for anything that needs it
+            if ((current_vert < staff_center - 23) || (current_vert > staff_center + 23))
+                draw_ledger_line(current_hor, current_vert);
+            // end ledger line block
+			
+			// read note duration here again from note_type which is set from duration
+			if      (note_type == 'w') draw_whole_note(current_hor, current_vert);
+            else if (note_type == 'h') draw_half_note(current_hor, current_vert);
+            else if (note_type == 'q') draw_quarter_note(current_hor, current_vert);
+            else if (note_type == 'e') draw_eighth_note(current_hor, current_vert);
+            else if (note_type == 's') draw_sixteenth_note(current_hor, current_vert);
         }
-        // end ledger line block
+		// end of note drawing block
+		
+		// keep this
+        wait_for_vsync();
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); 
 
-        // read note duration here again from note_type which is set from duration
-		if (note_type == 'w') {
-			draw_whole_note(current_hor, current_vert);
-		}
-		else if (note_type == 'h') {
-			draw_half_note(current_hor, current_vert);
-		}
-		else if (note_type == 'q') {
-			draw_quarter_note(current_hor, current_vert);
-		}
-		else if (note_type == 'e') {
-			draw_eighth_note(current_hor, current_vert);
-		}
-		else if (note_type == 's') {
-			draw_sixteenth_note(current_hor, current_vert);
-		}
-	}
-    // end of note drawing block
-				
-    // keep this
-	wait_for_vsync(); // swap front and back buffers on VGA vertical sync
-    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
-
-    // this should be the same as the other if statement with (note_drawn) except with incrementing current values
-    if (note_drawn) {
-        // add ledger line function for anything that needs it
-        if ((current_vert < staff_center - 23) || (current_vert > staff_center + 23)) {
-            draw_ledger_line(current_hor, current_vert);
+		// this should be the same as the other if statement with (note_drawn) except with incrementing current values
+        if (note_drawn) {
+            if ((current_vert < staff_center - 23) || (current_vert > staff_center + 23))
+                draw_ledger_line(current_hor, current_vert);
+            if (note_type == 'w') {
+                draw_whole_note(current_hor, current_vert);
+                current_hor += 120;
+            }
+            else if (note_type == 'h') {
+                draw_half_note(current_hor, current_vert);
+                current_hor += 60;
+            }
+            else if (note_type == 'q') {
+                draw_quarter_note(current_hor, current_vert);
+                current_hor += 30;
+            }
+            else if (note_type == 'e') {
+                draw_eighth_note(current_hor, current_vert);
+                current_hor += 30;
+            }
+            else if (note_type == 's') {
+                draw_sixteenth_note(current_hor, current_vert);
+                current_hor += 30;
+            }
+            note_drawn = false;
         }
-        // end ledger line block
-
-        // read note duration here again from note_type which is set from duration
-		if (note_type == 'w') {
-			draw_whole_note(current_hor, current_vert);
-			current_hor += 120;
-		}
-		else if (note_type == 'h') {
-			draw_half_note(current_hor, current_vert);
-			current_hor += 60;
-		}
-		else if (note_type == 'q') {
-			draw_quarter_note(current_hor, current_vert);
-			current_hor += 30;
-		}
-		else if (note_type == 'e') {
-			draw_eighth_note(current_hor, current_vert);
-			current_hor += 30;
-		}
-		else if (note_type == 's') {
-			draw_sixteenth_note(current_hor, current_vert);
-			current_hor += 30;
-		}
-    		
-		note_drawn = false;
-	}
-    // end of note drawing block
+		// end of note drawing block
+    }
 }
 
 
