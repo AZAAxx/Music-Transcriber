ebde7d4b1e8bdf3329d5cf6151fa0acde7e47c05
AZA <aza.xx269@gmail.com>
2026-03-25 13:24:18 -0400

    Wrote a windowing function and fixed the bug in find_note()

--- source/hal/PS2.h
+++ source/hal/PS2.h
@@ -1,8 +1,14 @@
 #ifndef __PS2__
 #define __PS2__
 
+#include <stdbool.h>
+
 volatile int * ps2_data_reg;
 volatile int * ps2_ctr_reg;
+
+bool break_code;  // true if F0 seen
+bool extended;     // true if E0 seen
+bool shift;        // true if shift is currently pressed
 
 
 void PS2_init();
